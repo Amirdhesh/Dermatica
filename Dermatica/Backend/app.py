83725dc21a1ea1b7d725feb4c42425fd06bd0d69
Amirdhesh <amirdhesh2003@gmail.com>
2024-01-13 17:26:30 +0530

    first commit

---
+++ Dermatica/Backend/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, jsonify
+from PIL import Image
+from Model import model_prediction
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/model", methods=["POST"])
+def model():
+        file = request.files["image"]
+        img = Image.open(file)
+        img.save("./image.jpg")
+        response = model_prediction()
+        if response['success']:
+            return jsonify(response)
+        else:
+            return jsonify(response)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
